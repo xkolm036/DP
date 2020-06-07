45c4267e1d6386f22651e14b45ba6d63b1439216
Martin Kolářík <XKOLM036@studenti.czu.cz>
2020-06-08 01:01:45 +0200

    Route seates fix

--- CarPool/CarPool/Controllers/CarsController.cs
+++ CarPool/CarPool/Controllers/CarsController.cs
@@ -12,7 +12,8 @@ using Microsoft.Extensions.Hosting.Internal;
 using System.IO;
 using Microsoft.AspNetCore.Hosting;
 using ImageMagick;
-
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace CarPool.Controllers
 {
@@ -38,23 +39,112 @@ namespace CarPool.Controllers
 
         }
 
+        private bool isCarValid(Car c)
+        {
+            bool validationState = true;
+            var nameRegex = new Regex(@"^[a-zA-Zá-žÁ-Ž]*$", RegexOptions.Compiled);
+            var czAlfabetWithNumer = new Regex(@"^[a-zA-Zá-žÁ-Ž0-9\s]*$", RegexOptions.Compiled);
+            var spzRegex = new Regex(@"^[A-Za-z0-9]{3}[\s-][0-9]{4}$", RegexOptions.Compiled);
+            var descriptionRegex = new Regex(@"^[a-zA-Zá-žÁ-Ž ?!.()[\]{}]*$", RegexOptions.Compiled);
+
+
+            if (c.brand != null)
+            {
+                if (!nameRegex.IsMatch(c.brand.Trim()))
+                {
+                    TempData["brand-error"] = "Špatně zadaný výrobce";
+                    validationState = false;
+                }
+            }
+            else
+            {
+                TempData["brand-error"] = "Vyýrobce není vyplněn";
+            }
+
+            if (c.color != null)
+            {
+                if (!nameRegex.IsMatch(c.color.Trim()))
+                {
+                    TempData["color-error"] = "Špatně zadaná barva";
+                    validationState = false;
+                }
+            }
+            else
+            {
+                TempData["color-error"] = "Není vyplněna barva";
+            }
+
+            if (c.model != null)
+            {
+                if (!czAlfabetWithNumer.IsMatch(c.model.Trim()))
+                {
+                    TempData["model-error"] = "Špatně vyplněn model auta";
+                    validationState = false;
+                }
+            }
+            else
+            {
+                TempData["model-error"] = "Není vyplněn model auta";
+            }
+
+            if (c.description != null)
+            {
+                if (!descriptionRegex.IsMatch(c.description))
+                {
+                    TempData["description-error"] = "Popis nesmí obsahovat speciální znaky";
+                }
+            }
+
+            if (c.seats <= 0)
+            {
+                TempData["seats-error"] = "Špatně vyplněn počet míst";
+            }
+
+            if (c.spz != null)
+            {
+                if (!spzRegex.IsMatch(c.spz))
+                {
+                    TempData["spz-error"] = "SPZ je ve špatném formátu";
+                }
+            }
+
+
+            return validationState;
+
+        }
+
+        private void compressImage(string path)
+        {
+            var file = new FileInfo(path);
+
+            Console.WriteLine("Bytes before: " + file.Length);
+
+            var optimizer = new ImageOptimizer();
+            optimizer.Compress(file);
+
+            file.Refresh();
+            Console.WriteLine("Bytes after:  " + file.Length);
+
+        }
+
+
         [Route("/Car/UploadImage")]
         public IActionResult UploadImage(IFormFile file, int id)
         {
-           
+
 
 
             Car c = new Car();
             c = _db.cars.Where(c => c.AppUser.Id == _userManager.GetUserId(User) && c.id == id).FirstOrDefault();
 
-                if (file.ContentType == "image/png" || file.ContentType == "image/jpeg")
+            if (file.ContentType == "image/png" || file.ContentType == "image/jpeg")
+            {
+                if (file.Length > 20971520)  //20 Mb
                 {
-                    if (file.Length > 20971520)  //20 Mb
-                    {
-                        TempData["msg-error"] = "Obrazek nesmi být větší než 20 MB";
-                    }
-                    else
-                    {
+                    TempData["msg-error"] = "Obrazek nesmi být větší než 20 MB";
+                }
+                else
+                {
 
                     var filePath = Path.GetTempFileName();
 
@@ -67,20 +157,20 @@ namespace CarPool.Controllers
                     //   optimizer.LosslessCompress(image);
                     //   image.CopyTo(filepath);
 
-                    file.CopyTo(new FileStream(filePath,FileMode.Create));
+                    file.CopyTo(new FileStream(filePath, FileMode.Create));
                     return Content(filePath);
 
                 }
 
 
 
-                }
-                else
-                {
-                    TempData["msg-error"] = "Je povolen pouze format png a jpg";
-                }
+            }
+            else
+            {
+                TempData["msg-error"] = "Je povolen pouze format png a jpg";
+            }
 
-            
+
             return Content("OK");
         }
 
@@ -106,7 +196,7 @@ namespace CarPool.Controllers
         public IActionResult GetCarSeats(int id)
         {
             Car carFromDb = new Car();
-            carFromDb = _db.cars.Where(c => c.id == id && c.AppUser.Id==_userManager.GetUserId(User)).FirstOrDefault();
+            carFromDb = _db.cars.Where(c => c.id == id && c.AppUser.Id == _userManager.GetUserId(User)).FirstOrDefault();
             if (carFromDb == null)
             {
                 ViewBag.seats = 0;
@@ -139,9 +229,20 @@ namespace CarPool.Controllers
         [HttpPost]
         public IActionResult AddCar(Car newCar, IFormFile file)
         {
+            List<Car> cars = new List<Car>();
+
+            //validate inputs and set error messages
+            if (!isCarValid(newCar))
+            {
+                cars.Add(newCar);
+                ViewData["Cars"] = cars;
+                return PartialView("_PartialGenerateCards");
+            }
+
+
             //try to find in db
             Car carFromDb = new Car();
-            carFromDb = _db.cars.Where(c => c.id == newCar.id && c.AppUser.Id==_userManager.GetUserId(User)).FirstOrDefault();
+            carFromDb = _db.cars.Where(c => c.id == newCar.id && c.AppUser.Id == _userManager.GetUserId(User)).FirstOrDefault();
 
             //modify existing car
             if (carFromDb != null)
@@ -154,48 +255,54 @@ namespace CarPool.Controllers
                 carFromDb.seats = newCar.seats;
                 carFromDb.spz = newCar.spz;
                 _db.SaveChanges();
+                carFromDb.AppUser = _db.Users.Where(u => u.Id == _userManager.GetUserId(User)).FirstOrDefault();
+
+                cars.Add(carFromDb);
             }
             else //new car
             {
-                newCar.AppUser = _db.Users.Where(u => u.Id == _userManager.GetUserId(User)).FirstOrDefault();
-                _db.cars.Add(newCar);
-                _db.SaveChanges();
-                ViewData["Cars"] = newCar;
+                  newCar.AppUser = _db.Users.Where(u => u.Id == _userManager.GetUserId(User)).FirstOrDefault();
+                  newCar.id = 0; //prevet to edit car of enother user
+                  _db.cars.Add(newCar);
+                  _db.SaveChanges();
+                  cars.Add(newCar);
             }
 
-            if (file.ContentType == "image/png" || file.ContentType == "image/jpeg")
+            if (file != null)
             {
-                if (file.Length > 20971520)  //20 Mb
+                if (file.ContentType == "image/png" || file.ContentType == "image/jpeg")
                 {
-                    TempData["msg-error"] = "Obrazek nesmi být větší než 20 MB";
+                    if (file.Length > 20971520)  //20 Mb
+                    {
+                        TempData["msg-error"] = "Obrazek nesmi být větší než 20 MB";
+                    }
+                    else
+                    {
+
+                        string filePath = Path.Combine(_env.WebRootPath, "images");
+                        string pripona = file.FileName.Split(".")[1];
+                        string filepath = Path.Combine(filePath, "image_" + newCar.id + "." + pripona);
+
+                        using (var stream = System.IO.File.Create(filepath))
+                        {
+                            file.CopyTo(stream);
+                        }
+                       // file.CopyTo(new FileStream(filepath, FileMode.Create));
+
+                        Task.Run(()=>compressImage(filepath));
+                    }
+
                 }
                 else
                 {
-
-                   string filePath = Path.Combine(_env.WebRootPath, "images");
-                   string pripona = file.FileName.Split(".")[1];
-                   string filepath = Path.Combine(filePath, "image_" + newCar.id + "."+pripona);
-
-
-                    //   var optimizer = new ImageOptimizer();
-                    //   optimizer.LosslessCompress(image);
-                    //   image.CopyTo(filepath);
-
-                    file.CopyTo(new FileStream(filepath, FileMode.Create));
-
-              
-
+                    TempData["msg-error"] = "Je povolen pouze format png a jpg";
                 }
-
-
-
-            }
-            else
-            {
-                TempData["msg-error"] = "Je povolen pouze format png a jpg";
             }
 
-            return Content(newCar.id.ToString());
+            // return Content(newCar.id.ToString());
+
+            ViewData["cars"] = cars;
+            return PartialView("_PartialGenerateCards");
         }
 
 
--- CarPool/CarPool/Controllers/RouteController.cs
+++ CarPool/CarPool/Controllers/RouteController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using CarPool.Data;
 using CarPool.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +37,7 @@ namespace CarPool.Controllers
         {
             ModelState.Clear(); // zamezi zobrazeni validačni hlašky po prvním spuštení
                                 //  return View("Clear");
+
             return View("FindRouteForm");
         }
 
@@ -129,23 +132,48 @@ namespace CarPool.Controllers
         [Authorize]
         [HttpPost]
         [Route("/Route/JoinRoute")]
-        public void JoinRoute(int id)
+        public string JoinRoute(int id)
         {
-            _db.routeUsers.Add(new RouteUser { RouteId = id, UserId = userManager.GetUserId(User) });
-            _db.SaveChanges();
+            Route routeFromDB = new Route();
+            routeFromDB = _db.routes.Where(r => r.id == id).FirstOrDefault();
+            if (routeFromDB != null)
+            {
+                //try if exists
+                RouteUser exists = _db.routeUsers.Where(ru => ru.RouteId == id && ru.UserId == userManager.GetUserId(User)).FirstOrDefault();
+                if (routeFromDB.seats > 0 && exists==null)
+                {
+                    routeFromDB.seats--;
+                    _db.SaveChanges();
+                    _db.routeUsers.Add(new RouteUser { RouteId = id, UserId = userManager.GetUserId(User) });
+                    _db.SaveChanges();
+                }
+                else
+                {
+                    TempData["msg-error"] = "Tato jízda je již plná";
+                }
+                
+            }
+            return "OK";
         }
 
         [Authorize]
         [HttpPost]
         [Route("/Route/LeaveRoute")]
-        public void LeaveRoute(int id)
+        public string LeaveRoute(int id)
         {
-            ViewData["Message"] = "Your application description page.";
-            Route routeFromDb = new Route();
+            var routeTodelete = _db.routeUsers.Where(ru => ru.RouteId == id && ru.UserId == userManager.GetUserId(User)).FirstOrDefault();
+            if (routeTodelete != null)
+            {
 
-                var routeTodelete = _db.routeUsers.Where(ru => ru.RouteId == id && ru.UserId == userManager.GetUserId(User)).FirstOrDefault();
+                Route r = _db.routes.Where(ro => ro.id == id).FirstOrDefault();
+                r.seats++;
+
                 _db.routeUsers.Remove(routeTodelete);
                 _db.SaveChanges();
+            }
+            else
+                TempData["msg-error"] = "Došlo k chybě při odhlašování";
+            return "OK";
         }
 
         [Authorize]
@@ -153,22 +181,22 @@ namespace CarPool.Controllers
         public IActionResult ShowLoggedRoutes()
         {
             List<Route> myRoutes = new List<Route>();
-       
-
-       
-                var query = from RouteUser in _db.routeUsers
-                            join route in _db.routes on RouteUser.RouteId equals route.id
-                            where RouteUser.UserId == userManager.GetUserId(User) && route.id == RouteUser.RouteId
-                            select route;
 
 
-                foreach (Route routeFromQuery in query)
-                {
-                    routeFromQuery.connected = true;
-                    myRoutes.Add(routeFromQuery);
-                }
 
-            
+            var query = from RouteUser in _db.routeUsers
+                        join route in _db.routes on RouteUser.RouteId equals route.id
+                        where RouteUser.UserId == userManager.GetUserId(User) && route.id == RouteUser.RouteId
+                        select route;
+
+
+            foreach (Route routeFromQuery in query)
+            {
+                routeFromQuery.connected = true;
+                myRoutes.Add(routeFromQuery);
+            }
+
+
 
             ViewData["Routes"] = myRoutes;
             ViewData["Title"] = "Přihlášené jízdy";
@@ -187,15 +215,15 @@ namespace CarPool.Controllers
         {
             List<Route> createtByUser = new List<Route>();
 
-       
-                var query = _db.routes.Where(r => r.CreatedBy.Id == userManager.GetUserId(User));
 
-                foreach (Route route in query)
-                {
-                    route.connected = null;
-                    createtByUser.Add(route);
-                }
-         
+            var query = _db.routes.Where(r => r.CreatedBy.Id == userManager.GetUserId(User));
+
+            foreach (Route route in query)
+            {
+                route.connected = null;
+                createtByUser.Add(route);
+            }
+
             ViewData["Routes"] = createtByUser;
             ViewData["Title"] = "Vytvořené jízdy";
 
@@ -213,24 +241,28 @@ namespace CarPool.Controllers
         {
 
 
-                Route ro = new Route();
-                ro = _db.routes.Where(r => r.id == id).FirstOrDefault();
+            Route ro = new Route();
+            ro = _db.routes.Where(r => r.id == id && r.CreatedBy.Id == userManager.GetUserId(User)).FirstOrDefault();
 
-                if (ro.CreatedBy.Id.Equals(userManager.GetUserId(User)))
+            if (ro != null)
+            {
+
+                var routesToDelete = _db.routeUsers.Where(r => r.RouteId == id);
+                foreach (RouteUser ru in routesToDelete)
                 {
-
-                    var routesToDelete = _db.routeUsers.Where(r => r.RouteId == id);
-                    foreach (RouteUser ru in routesToDelete)
-                    {
-                        _db.Remove(ru);
-                    }
-
-
-                    ro = _db.routes.Where(r => r.id == id && r.CreatedBy.Id == userManager.GetUserId(User)).FirstOrDefault();
-                    _db.routes.Remove(ro);
-                    _db.SaveChanges();
+                    _db.Remove(ru);
                 }
-          
+
+
+                ro = _db.routes.Where(r => r.id == id && r.CreatedBy.Id == userManager.GetUserId(User)).FirstOrDefault();
+                _db.routes.Remove(ro);
+                _db.SaveChanges();
+            }
+            else
+            {
+                TempData["msg-error"] = "Nastala chyba při mazaní jízdy";
+            }
+
 
 
         }
